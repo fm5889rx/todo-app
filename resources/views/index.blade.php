e6c7e398b02c52ca15f13ba76a0e93c639d64e92
ToshiM <fm5889.rx@gmail.com>
2026-05-07 09:39:53 +0900

    テーブルの作成 #3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,9 @@
 </div>
 
 <div class="todo__content">
-    <form class="create-form">
+    <form class="create-form" action="/todos" method="POST">
+        @csrf
+
         <div class="create-form__item">
             <input class="create-form__item-input" type="text" name="content">
         </div>
@@ -29,26 +31,9 @@
                 <td class="todo-table__item">
                     <form class="update-form">
                         <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" name="content" value="test">
-                        </div>
-                        <div class="update-form__button">
-                            <button class="update-form__button-submit" type="submit">更新</button>
-                        </div>
-                    </form>
-                </td>
-                <td class="todo-table__item">
-                    <form class="delete-form">
-                        <div class="delete-form__button">
-                            <button class="delete-form__button-submit" type="submit">削除</button>
-                        </div>
-                    </form>
-                </td>
-            </tr>
-            <tr class="todo-table__row">
-                <td class="todo-table__item">
-                    <form class="update-form">
-                        <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" name="content" value="test2">
+                            @foreach ($todos as $todo)
+                                <input class="update-form__item-input" type="text" name="content">{{ $todo['content'] }}</input>
+                            @endforeach
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
